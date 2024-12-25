4628760a4277f44aec53847f184a7585e5da0733
hatimbelghiti <belghitihatim00@gmail.com>
2024-12-25 15:07:51 +0100

    adding diagramme de class

--- view/user_dashboard.php
+++ view/user_dashboard.php
@@ -29,7 +29,7 @@
             <div class="bg-white p-6 rounded-lg shadow-md">
                 <h2 class="text-xl font-semibold mb-4">Mes Réservations</h2>
                 <div id="reservationsList" class="space-y-4">
-                    <!-- Reservations will be loaded here -->
+                    <!-- reservations will be loaded here -->
                 </div>
             </div>
 
