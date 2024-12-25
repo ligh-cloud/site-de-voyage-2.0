e1d5af2cdeb4285fc6123ded0e9f4cf47c792878
hatimbelghiti <belghitihatim00@gmail.com>
2024-12-25 12:59:18 +0100

    adding sql code

--- db_create.sql
+++ db_create.sql
@@ -1,18 +1,40 @@
-create database travel;
+
+
+CREATE DATABASE travel;
 USE travel;
+
 CREATE TABLE utilisateurs(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(150) NOT NULL,
     prenom VARCHAR(150) NOT NULL,
-    email VARCHAR(200) UNIQUE NOT NULL,
-    
+    email VARCHAR(200) UNIQUE NOT NULL
 );
+
 CREATE TABLE activite(
-    id_activite INT AUTO_INCREMENT PRIMARY KEY, 
-    id_user INT,
+    id_activite INT AUTO_INCREMENT PRIMARY KEY,
     vols VARCHAR(150) NOT NULL,
-    hotels VARCHAR(150) , NOT NULL,
-    circuits_touristiques TEXT ,
-    FOREIGN KEY id_user REFFERENCE
+    hotels VARCHAR(150) NOT NULL,
+    circuits_touristiques TEXT,
+    titre VARCHAR(150) NOT NULL,
+    prix DECIMAL(10,2),
+    date_debut DATE,
+    date_fin DATE
+);
+
+CREATE TABLE reservation(
+    id_reservation INT(11) AUTO_INCREMENT PRIMARY KEY,
+    id_client INT(11),
+    id_activite INT(11) NOT NULL,
+    date_reservation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('en attente', 'confirmer', 'annuler') DEFAULT 'en attente',
+    FOREIGN KEY (id_client) REFERENCES utilisateurs(id),
+    FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
+);
+
+CREATE TABLE roles(
+    id_role INT AUTO_INCREMENT PRIMARY KEY,
+    id_client INT,
+    FOREIGN KEY (id_client) REFERENCES utilisateurs(id),
+    role ENUM('admin', 'user')
 );
 
